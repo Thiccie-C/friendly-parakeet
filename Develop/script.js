eed1c90ba6f901dc34692640bf73c03b37794975
Thiccie-C <Nelsonnoremac@gmail.com>
2021-12-12 22:30:18 -0600

    added the password functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,7 @@
 // Assignment code here
-var specialCharacters = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-var numbers = "1234567890";
-var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerCase ="abcdefghijklmnopqrstuvwxyz"
+
+// Get references to the #generate element
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,11 +9,41 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var generatePassword = function() {
+  var passwordLength = window.prompt("How many characters long do you want your Password to be?")
+  if(passwordLength >= 8 && passwordLength <= 128){
+    var passwordLengthConfirm = window.confirm("You want a password with " + passwordLength + " characters?");
+}  
+else{
+      window.alert("Your password Needs to be between 8 characters and 128 characters")
+      writePassword();
+    }
+  var lowerCaseLetters="abcdefghijklmnopqrstuvwxyz",
+  upperCaseLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  numbers="123456789",
+  specialCharacters = 
+  password='';
+var length = Math.ceil(passwordLength/2);
+length = length - 1;
+var lenSpec = passwordLength-2*length;
+
+for (i=0;i<length;i++) {
+  password+=lowerCaseLetters.charAt(Math.floor(Math.random()*lowerCaseLetters.length));
+  password+=numbers.charAt(Math.floor(Math.random()*numbers.length));
+  password+=upperCaseLetters.charAt(Math.floor(Math.random()*upperCaseLetters.length))
+}
+
+for (i=0;i<lenSpec;i++)
+  password+=specialCharacters.charAt(Math.floor(Math.random()*specialCharacters.length));
+
+password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+return password;
+
+};
